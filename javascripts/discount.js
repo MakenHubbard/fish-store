cad21c9612431b98c4d6d3af2b503c7b85bbba54
Maken Hubbard <makhub26@outlook.com>
2018-05-01 21:11:14 -0500

    have the remove from cart button executing

---
+++ javascripts/discount.js
@@ -0,0 +1,13 @@
+const discount = 0.2;
+
+const applySale = () => {
+  $('.on-sale').each((i, fishCard) => {
+    console.log($(fishCard).find('.caption'));
+    const basePriceElem = $(fishCard).find('.price');
+    const basePrice = basePriceElem.html() * 1;
+    const newPrice = (basePrice * (1 - discount)).toFixed(2);
+    basePriceElem.html(newPrice);
+  });
+};
+
+module.exports = applySale;
